f6e09751323598fd141398c38c301c06bd65f965
Frederic M <fred.madranges@gmail.com>
2026-01-21 15:30:56 +0100

    add strategie scss

--- src/app/features/shop/pages/product-details/product-details.component.ts
+++ src/app/features/shop/pages/product-details/product-details.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { SimuleLourdComponent } from './simuleLourdComponent/simuleLourdComponent';
 
 @Component({
   standalone: true,
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, SimuleLourdComponent],
 })
 export class ProductDetailsComponent {
   productId: string | null = null;
---
+++ src/app/features/shop/pages/product-details/simuleLourdComponent/simuleLourdComponent.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+function burnCpu(ms = 800) {
+  const end = performance.now() + ms;
+  let x = 0;
+  while (performance.now() < end) {
+    // Calculs arbitraires
+    x = Math.sin(x + Math.random()) * Math.cos(x + 1);
+  }
+  return x;
+}
+@Component({
+  selector: 'app-simule-lourd-component',
+  imports: [],
+  template: `<p>CPU burn done: {{ result }}</p>`,
+  styleUrl: './simuleLourdComponent.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SimuleLourdComponent {
+  result = burnCpu(2000);
+ }
